d6579ca06ceea36328e4df60ec706dc9f9e1e81b
DoctorShields <matthew.shields@charlottesvilleschools.org>
2020-10-12 10:22:15 -0400

    adding led blink

---
+++ led_blink_blue_yellow.py
@@ -0,0 +1,15 @@
+import board
+import neopixel
+import time
+
+# make a neopixel object
+dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
+
+# this will run forever
+while True:
+    print("Make it blue!")
+    dot.fill((0,0,255))
+    time.sleep(.5)
+    print("Make it yellow!")
+    dot.fill((255,255,0))
+    time.sleep(.5)

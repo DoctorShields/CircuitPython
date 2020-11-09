0e893af2c82cf2374db6b80a109346b195e9d329
DoctorShields <matthew.shields@charlottesvilleschools.org>
2020-11-09 15:53:00 -0500

    adding lcd touch counter

---
+++ lcd_touch_counter.py
@@ -0,0 +1,37 @@
+import touchio
+#import digitalio
+#import analogio
+#import neopixel
+import board
+import time
+#import servo
+#import busio
+
+from lcd.lcd import LCD
+from lcd.i2c_pcf8574_interface import I2CPCF8574Interface
+lcd = LCD(I2CPCF8574Interface(0x3f), num_rows=2, num_cols=16)
+
+touchBtn = touchio.TouchIn(board.A0)
+touchToggle = touchio.TouchIn(board.A1)
+lastTouch = False
+lastToggle = False
+up = True
+touchCount = 0
+
+lcd.print("Touches | Up/Dn")
+lcd.set_cursor_pos(1, 0)
+lcd.print("0       | Up")
+
+while True:
+    if touchBtn.value and not lastTouch:
+        touchCount = touchCount + 1 if up else touchCount - 1
+        lcd.set_cursor_pos(1, 0)
+        lcd.print(str(touchCount)+"   ")
+    if touchToggle.value and not lastToggle:
+        up = not up
+        lcd.set_cursor_pos(1, 8)
+        msg = "| Up" if up else "| Dn"
+        lcd.print(msg)
+    lastTouch = touchBtn.value
+    lastToggle = touchToggle.value
+    time.sleep(0.05)
